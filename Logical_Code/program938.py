203fabaf08939a847a137f9a7da6ecd3edab9c4e
Rahul Ugale <rugale980@gmail.com>
2026-04-01 13:00:34 +0530

    Add files via upload

---
+++ Logical_Code/program938.py
@@ -0,0 +1,12 @@
+# Generic
+def Addition(A,B):
+    Result = 0
+    Result = A + B
+    return Result
+    
+def main():
+    print(Addition(10,11))
+    print(Addition(10.3,11.2))
+    print(Addition("Hello","World"))
+   
+main()      
