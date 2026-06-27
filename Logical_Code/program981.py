98cae0889bb8f4e4aea413a6e96f0ea426008693
Rahul Ugale <rugale980@gmail.com>
2026-06-27 14:03:44 +0530

    Add files via upload

---
+++ Logical_Code/program981.py
@@ -0,0 +1,12 @@
+def Addition(A,B):
+    return A + B
+        
+def main():
+
+    print("Inside Main Function")
+    
+    Ret = Addition(10,11)
+    print("Addition is : ",Ret)
+    
+if __name__ == "__main__":    
+    main()      
---
+++ Logical_Code/program982.py
@@ -0,0 +1,10 @@
+# include<Sample.h>
+# using namespace Sample
+# import Sample.*;
+
+import Sample
+
+print("Inside Main Application")
+print("__name__ from main application is : ",__name__)
+
+Sample.Display()
