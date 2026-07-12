6225d4cad5f8ea977d03bc2b50cbce5c728413fe
Rahul Ugale <rugale980@gmail.com>
2026-07-12 11:57:43 +0530

    Add files via upload

---
+++ Logical_Code/program988.py
@@ -0,0 +1,60 @@
+# Singly Linear Linked List
+# Done
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+        
+class SinglyLL:
+    # Done
+    def __init__(self):
+        self.first = None 
+        self.iCount = 0 
+        
+    def InsertFirst(self,no):
+        pass 
+    
+    def InsertLast(self,no):
+        pass  
+    
+    def InsertAtPos(self,no,pos):
+        pass
+    
+    def DeleteFirst(self):
+        pass
+    
+    def DeleteLast(self):
+        pass
+    
+    def DeleteAtPos(self,pos):
+        pass
+    
+    # Done
+    def Display(self):
+        temp = self.first
+        
+        while(temp != None):
+            print("| ",temp.data," |->",end=" ")
+            temp = temp.next 
+         
+        print("None")      
+    
+    # Done
+    def Count(self):
+        return self.iCount     
+        
+def main():
+    sobj = SinglyLL()
+    
+    sobj.InsertFirst(101)
+    sobj.InsertFirst(51)
+    sobj.InsertFirst(21)
+    sobj.InsertFirst(11)
+    
+    print("Elements of Linked List are : ")
+    sobj.Display()
+    
+    print("Number of elements in Linked List are : ",sobj.Count())
+    
+if __name__ == "__main__":
+    main()
